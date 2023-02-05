926901751f9c669b4038cd164f86188f67dbc8c8
Artur vinicius umbelino barbalho <arthurvinicius907@gmail.com>
2023-02-05 07:27:03 -0300

    routing finished

--- public/index.php
+++ public/index.php
@@ -1,5 +1,22 @@
 <?php
 include("../core/classes/Functions.php");
+$routes = [
+    'home',
+    'about'
+];
+if(empty($_GET)){
+    Functions::Layout('home'); 
+}
+foreach($routes as $route){
+    if(isset($_GET[$route])){
+        if(isset($_GET['parameter'])){
+            $parameter = $_GET['parameter'];
+            Functions::Layout($route,$parameter);
+        }
+        else{
+            Functions::Layout($route);  
+        }
+    } 
+}
 
-Functions::Layout('about');
 
