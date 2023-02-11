bad81ebfe756336e3b7b2f46f7eba285bc0b18d9
Artur vinicius umbelino barbalho <arthurvinicius907@gmail.com>
2023-02-10 22:22:25 -0300

    css finished

--- config.php
+++ config.php
@@ -5,3 +5,6 @@ const MYSQL_DATABASE = 'store';
 const MYSQL_USER = 'root';
 const MYSQL_PASS = '';
 const MYSQL_CHARSET = 'utf8';
+
+const MAIL = 'hijikatakun32@gmail.com';
+const MAIL_PASS = 'd_A385xrSQ_Z75U';
--- public/assets/css/S.home.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Content-type: text/css; charset:UFT-8');
-?>
